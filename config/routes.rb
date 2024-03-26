874d07e9ebf860e50bee3bda03a4191c6c98cb05
ninamaleckosak <156948856+ninamaleckosak@users.noreply.github.com>
2024-03-26 15:53:04 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/articles", to: "articles#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
