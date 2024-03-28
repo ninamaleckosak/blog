1f8b976fb74d4a9d742bb4c28bd4841dc4adeda1
ninamaleckosak <156948856+ninamaleckosak@users.noreply.github.com>
2024-03-28 15:23:11 -0400

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,4 @@
 class Article < ApplicationRecord
-<<<<<<< HEAD
-=======
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
->>>>>>> d3da079ec74dee39d1115b6ab08779374088ef51
 end
