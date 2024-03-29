bf27e43ee5299374c49ae5d62fabc4839d8ebb06
ninamaleckosak <156948856+ninamaleckosak@users.noreply.github.com>
2024-03-28 21:10:11 -0400

    Update visible.rb

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -6,6 +6,12 @@ module Visible
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
 
   def archived?
     status == 'archived'
