3a67063e34dc7d335c056010f00f0a03998f78d9
ninamaleckosak <156948856+ninamaleckosak@users.noreply.github.com>
2024-04-11 15:47:46 -0400

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "jps", password: "kenyon", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
